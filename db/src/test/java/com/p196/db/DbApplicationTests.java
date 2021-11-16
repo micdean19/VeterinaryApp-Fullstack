b93ae09c229b3463b0bd29986fd33d53fe28f0cb
Alex Leakos <aleakos@gmail.com>
2021-11-16 10:42:49 -0700

    inital maven

---
+++ db/src/test/java/com/p196/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p196.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
